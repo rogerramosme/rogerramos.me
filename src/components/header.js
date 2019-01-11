ac0e2be551eb27355be005fce49a7c871b282020
Roger Ramos <rodgerpaulo@hotmail.com>
2019-01-11 02:45:44 -0200

    Does some styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,10 @@
-import { string } from 'prop-types'
-import React from 'react'
+import { string } from "prop-types";
+import React from "react";
 
 const Header = ({ siteTitle, siteSubTitle }) => (
   <div
     style={{
-      marginBottom: `1.45rem`,
+      marginBottom: `1.45rem`
     }}
   >
     <div
@@ -12,20 +12,29 @@ const Header = ({ siteTitle, siteSubTitle }) => (
         margin: `0 auto`,
         maxWidth: 960,
         padding: `4rem 1.0875rem 1.45rem`,
-        textAlign: 'center'
+        textAlign: "center"
       }}
     >
-      <h1 style={{ margin: 0, lineHeight: 1.125, fontSize: '2rem', lineBreak: 'break-word' }}>
-          {siteTitle}
+      <h1
+        style={{
+          margin: 0,
+          lineHeight: 1.125,
+          fontSize: "2rem",
+          lineBreak: "break-word"
+        }}
+      >
+        {siteTitle}
       </h1>
-      <h2 style={{ fontWeight: '400', lineHeight: 1.5, fontSize: '1.25rem' }}>{siteSubTitle}</h2>
+      <h2 style={{ fontWeight: "400", lineHeight: 1.5, fontSize: "1.25rem" }}>
+        {siteSubTitle}
+      </h2>
     </div>
   </div>
-)
+);
 
 Header.propTypes = {
   siteTitle: string.isRequired,
   siteSubTitle: string.isRequired
-}
+};
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Layout from '../components/layout'
-import ProfileImage from '../components/profile-image'
-import SEO from '../components/seo'
+import React from "react";
+import Layout from "../components/layout";
+import ProfileImage from "../components/profile-image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Roger Ramos" keywords={[`design`, `front-end`, `react`, `gatsby`, `react`]} />
-    <div style={{ marginBottom: `1.45rem`, display: 'inline' }}>
+    <SEO
+      title="Roger Ramos"
+      keywords={[`design`, `front-end`, `react`, `gatsby`, `react`]}
+    />
+    <div style={{ marginBottom: `1.45rem`, display: "inline" }}>
       <ProfileImage />
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
